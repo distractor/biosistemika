dce6d54deeaa92a238a6e9d7a958bfe43d568eec
Mitja Jancic <mitjajancic@gmail.com>
2023-09-21 09:06:26 +0200

    Printed output.

--- Classes/Plate.cs
+++ Classes/Plate.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using Newtonsoft.Json;
 
 namespace bs_plates.Classes
@@ -7,12 +9,13 @@ namespace bs_plates.Classes
     public class Plate
     {
         private int _plateSize;
-        private List<List<string>> _experiments;
+        private List<List<string>> _samples;
         private List<List<string>> _reagents;
         private List<int> _replications;
         private int _maxPlates;
 
-        private static readonly List<int> _allowedPlateSizes = new List<int> { 96, 384 };
+        private static readonly List<int> AllowedPlateSizes = new List<int> { 96, 384 };
+        private List<Well> _wells = new List<Well>();
 
         public Plate()
         {
@@ -22,6 +25,7 @@ namespace bs_plates.Classes
             PrintInputs();
 
             // Optimize setup.
+            Preprocess();
             OptimizeSetup();
         }
 
@@ -31,15 +35,29 @@ namespace bs_plates.Classes
         private void GetInputs()
         {
             // Read plate size.
-            _plateSize = InputReader.GetInteger("Enter plate size: ", _allowedPlateSizes);
+            // _plateSize = InputReader.GetInteger("Enter plate size: ", AllowedPlateSizes);
+            _plateSize = 96;
             // Read experiment names.
-            _experiments = InputReader.GetJsonString<List<List<string>>>("Enter experiment names: ");
+            // _samples = InputReader.GetJsonString<List<List<string>>>("Enter sample names: ");
+            _samples = new List<List<string>>
+            {
+                new List<string> { "Sample-1", "Sample-2", "Sample-3" },
+                new List<string> { "Sample-1", "Sample-2", "Sample-3" }
+            };
+
             // Read reagents.
-            _reagents = InputReader.GetJsonString<List<List<string>>>("Enter reagents: ");
+            // _reagents = InputReader.GetJsonString<List<List<string>>>("Enter reagents: ");
+            _reagents = new List<List<string>>
+            {
+                new List<string> { "pink" },
+                new List<string> { "green" }
+            };
             // Read replications.
-            _replications = InputReader.GetJsonString<List<int>>("Enter replication list: ");
+            // _replications = InputReader.GetJsonString<List<int>>("Enter replication list: ");
+            _replications = new List<int> { 3, 2 };
             // Read max number of plates.
-            _maxPlates = InputReader.GetInteger("Enter maximum number of plates: ", null);
+            // _maxPlates = InputReader.GetInteger("Enter maximum number of plates: ", null);
+            _maxPlates = 1;
         }
 
         /// <summary>
@@ -49,7 +67,7 @@ namespace bs_plates.Classes
         {
             Console.WriteLine("Your input: ");
             Console.WriteLine("  - plate size: {0}", _plateSize);
-            Console.WriteLine("  - experiment names: {0}", JsonConvert.SerializeObject(_experiments));
+            Console.WriteLine("  - sample names: {0}", JsonConvert.SerializeObject(_samples));
             Console.WriteLine("  - reagent names: {0}", JsonConvert.SerializeObject(_reagents));
             Console.WriteLine("  - replications: {0}", JsonConvert.SerializeObject(_replications));
             Console.WriteLine("  - maximum number of plates: {0}", _maxPlates);
@@ -58,6 +76,86 @@ namespace bs_plates.Classes
         private void OptimizeSetup()
         {
             Console.WriteLine("Optimizing ....");
+
+            // Set plate size.
+            var nRows = (_plateSize == 96) ? 8 : 12;
+            var nCols = 2 * nRows;
+            var wellMatrix = new Well[nRows, nCols];
+
+            int row = 0, col = 0;
+
+            for (var experimentIndex = 0; experimentIndex < _replications.Count; experimentIndex++)
+            {
+                var samples = _samples[experimentIndex];
+                var reagents = _reagents[experimentIndex];
+                var replicationSize = _replications[experimentIndex];
+
+                // Iterate over all samples.
+                foreach (var sampleName in samples)
+                {
+                    foreach (var reagentName in reagents)
+                    {
+                        for (var repCount = 0; repCount < replicationSize; repCount++)
+                        {
+                            var well = new Well(sampleName, reagentName,
+                                string.Format("{0}/{1}", repCount + 1, replicationSize), new Tuple<int, int>(row, col));
+
+                            wellMatrix[row, col] = well;
+                            col++;
+                        }
+                    }
+
+                    row++;
+                    col = 0;
+                }
+            }
+
+            Postprocess(wellMatrix);
+        }
+
+        private void Preprocess()
+        {
+            Console.WriteLine("Preprocessing ...");
+        }
+
+        private void Postprocess(Well[,] wells)
+        {
+            Console.WriteLine("hI");
+            for (var row = 0; row < wells.GetLength(0); row++)
+            {
+                for (int col = 0; col < wells.GetLength(1); col++)
+                {
+                    if (wells[row, col] != null)
+                    {
+                        _wells.Add(wells[row, col]);
+                    }
+                }
+            }
+
+            PrintPlate();
+        }
+
+        private void PrintPlate()
+        {
+            Console.WriteLine("Postprocessing ...");
+            var sortedWells = _wells.OrderBy(w => w.Location.Item1)
+                .ThenBy(w => w.Location.Item2)
+                .ToList();
+
+            for (var i = 0; i < sortedWells.Count; i++)
+            {
+                if (i > 0)
+                {
+                    if (sortedWells[i - 1].Location.Item1 != sortedWells[i].Location.Item1)
+                    {
+                        Console.Write("\n");
+                    }
+                }
+
+                Console.Write("{0}; ", sortedWells[i]);
+            }
+
+            Console.Write("\n");
         }
     }
 }
---
+++ Classes/Well.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace bs_plates.Classes
+{
+    public class Well
+    {
+        public string Sample;
+        public string Reagent;
+        public string ReplicationId;
+        public Tuple<int, int> Location;
+
+        public Well()
+        {
+            Sample = null;
+            Reagent = null;
+            ReplicationId = null;
+            Location = null;
+        }
+
+        public Well(string sampleName, string reagentName, string replicationString, Tuple<int, int> plateLocation)
+        {
+            Sample = sampleName;
+            Reagent = reagentName;
+            ReplicationId = replicationString;
+            Location = plateLocation;
+        }
+
+        public override string ToString()
+        {
+            var text = string.Format("{0},{1},{2},({3},{4})", Sample, Reagent, ReplicationId, Location.Item1,
+                Location.Item2);
+            return text;
+        }
+    }
+}
